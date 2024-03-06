13c16d10c482cd2dfa5bbf2b85f523e1f0a76aeb
HanaXasro <88589183+HanaXasro@users.noreply.github.com>
2024-03-06 23:32:06 +0300

    Add project files.

---
+++ Application/Services/PesonalInfo.cs
@@ -0,0 +1,38 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Application.Services
+{
+    public class PesonalInfo
+    {
+
+
+        public object Response { get; set; }
+
+        public PesonalInfo()
+        {
+
+            // Name Users in folder local C:\Users\
+            var ArrayUsername = WindowsIdentity.GetCurrent().Name;
+            if (ArrayUsername!=null)
+            {
+                string[] user = ArrayUsername.Split("\\");
+                string userName = user[1]; // or host name GetHostName()
+                Response = new
+                {
+                    IpAddressV4 = Dns.GetHostEntry(Dns.GetHostName()).AddressList.GetValue(1)!.ToString()!,
+                    userName = userName,
+                    password = BC.HashPassword("1234")
+                };
+            }
+        }
+    }
+}
---
+++ StartUpApplication/Controllers/PersonalInfoController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartUpApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonalInfoController : ControllerBase
+    {
+        private readonly PesonalInfo pesonalInfo;
+
+        public PersonalInfoController(PesonalInfo pesonalInfo)
+        {
+            this.pesonalInfo = pesonalInfo;
+        }
+
+        [HttpGet]
+        public IActionResult GetPcName()
+        {
+
+
+            return Ok(pesonalInfo);
+        }
+    }
+}
